8511d1bfdb51d702d7036410315c6812f209ab0d
Oren (electricessence) <oren@electrified.net>
2017-09-24 15:40:18 -0700

    Updates

--- PrimeNumbers.cs
+++ PrimeNumbers.cs
@@ -6,150 +6,156 @@ using Open.Collections;
 
 public class PrimeNumbers
 {
-    protected static IEnumerable<ulong> ValidPrimeTests()
-    {
-        yield return 2;
-        for (ulong n = 3; n < ulong.MaxValue - 1; n += 2)
-            yield return n;
-    }
+	protected static IEnumerable<ulong> ValidPrimeTests()
+	{
+		yield return 2;
+		for (ulong n = 3; n < ulong.MaxValue - 1; n += 2)
+			yield return n;
+	}
 
-    public ParallelQuery<ulong> Values()
-    {
-        return ValidPrimeTests()
-            .AsParallel().AsOrdered()
-            .Where(v => IsPrime(v));
-    }
+	public virtual ParallelQuery<ulong> Values()
+	{
+		return ValidPrimeTests()
+			.AsParallel().AsOrdered()
+			.Where(v => IsPrime(v));
+	}
 
-    public bool IsPrime(ulong value)
-    {
-        switch (value)
-        {
-            // 0 and 1 are not prime numbers
-            case 0:
-            case 1:
-                return false;
+	public IEnumerable<KeyValuePair<ulong,ulong>> IndexedValues() // 1 is the starting index.
+	{
+		ulong count = 0;
+		foreach(var v in Values())
+			yield return KeyValuePair.Create(++count,v);
+	}
 
-            case 2:
-            case 3:
-                return true;
+	public bool IsPrime(ulong value)
+	{
+		switch (value)
+		{
+			// 0 and 1 are not prime numbers
+			case 0:
+			case 1:
+				return false;
 
-            default:
-                return value % 2 != 0
-                    && value % 3 != 0
-                    && IsPrimeInternal(value);
-        }
+			case 2:
+			case 3:
+				return true;
 
-    }
+			default:
+				return value % 2 != 0
+					&& value % 3 != 0
+					&& IsPrimeInternal(value);
+		}
 
-    public bool IsPrime(long value)
-    {
-        return IsPrime((ulong)Math.Abs(value));
-    }
+	}
 
-    protected virtual bool IsPrimeInternal(ulong value)
-    {
-        if (value < 380000)
-        {
-            // This method is faster up until a point.
-            double squared = Math.Sqrt(value);
-            ulong flooredAndSquared = Convert.ToUInt64(Math.Floor(squared));
+	public bool IsPrime(long value)
+	{
+		return IsPrime((ulong)Math.Abs(value));
+	}
 
-            for (ulong idx = 3; idx <= flooredAndSquared; idx++)
-            {
-                if (value % idx == 0)
-                {
-                    return false;
-                }
-            }
-        }
-        else
-        {
-            ulong divisor = 6;
-            while (divisor * divisor - 2 * divisor + 1 <= value)
-            {
+	protected virtual bool IsPrimeInternal(ulong value)
+	{
+		if (value < 380000)
+		{
+			// This method is faster up until a point.
+			double squared = Math.Sqrt(value);
+			ulong flooredAndSquared = Convert.ToUInt64(Math.Floor(squared));
 
-                if (value % (divisor - 1) == 0)
-                    return false;
+			for (ulong idx = 3; idx <= flooredAndSquared; idx++)
+			{
+				if (value % idx == 0)
+				{
+					return false;
+				}
+			}
+		}
+		else
+		{
+			ulong divisor = 6;
+			while (divisor * divisor - 2 * divisor + 1 <= value)
+			{
 
-                if (value % (divisor + 1) == 0)
-                    return false;
+				if (value % (divisor - 1) == 0)
+					return false;
 
-                divisor += 6;
-            }
-        }
+				if (value % (divisor + 1) == 0)
+					return false;
+
+				divisor += 6;
+			}
+		}
 
 
 
-        return true;
-    }
+		return true;
+	}
 
-    public class BruteForce : PrimeNumbers
-    {
-        IEnumerable<ulong> ValuesInternal()
-        {
-            return ValidPrimeTests()
-                // .AsParallel()
-                // .AsOrdered()
-                .Where(n =>
-                {
-					// Provide enough numbers for critical mass.
-                    switch (n)
-                    {
-                        case 2:
-                        case 3:
-						case 5:
-						case 7:
-                        case 11:
-                        case 13:
-                            return true;
-                        
-                        case 9:
-                            return false;
+	public class BruteForce : PrimeNumbers
+	{
+		IEnumerable<ulong> _values;
+		public new IEnumerable<ulong> Values()
+		{
+			return LazyInitializer.EnsureInitialized(ref _values,
+				()=> ValidPrimeTests()
+					.Where(n =>
+					{
+						switch(n)
+						{
+							case 2:
+							case 3:
+							case 5:
+							case 7:
+							case 11:
+							case 13:
+							case 17:
+							case 19:
+							case 23:
+								return true;
+						}
 
-                        default:
-                            ulong half = n / 2; // Don't search past the 1/2 value of primes since we already know it can't be divided by 2.
-                            return Values()
-								.Skip(2)
-                                .TakeWhile(v => v <= half)
-                                .All(v => (n % v) != 0);
-                    }
-                });
-        }
+						if(n<23) return false;
 
-        LazyList<ulong> _values;
-        public new IEnumerable<ulong> Values()
-        {
-            return LazyInitializer.EnsureInitialized(ref _values,
-                () => ValuesInternal().Memoize(true));
-        }
+						ulong last = 1;
+						// Recursion can occur here but that's okay!!! Uses predecessors to acquire this value.
+						foreach(var v in Values()) 
+						{
+							ulong stop = n / last; // The list of possibilities shrinks for each test.
+							if (v > stop) break; // Exceeded possibilities? 
+							if((n % v) == 0) return false;
+							last = v;
+						}
 
-        protected override bool IsPrimeInternal(ulong value)
-        {
-            return Values()
-                .Skip(2) // 2 & 3
-                .TakeWhile(p => p <= value)
-                .Contains(value);
-        }
+						return true;
+					}).Memoize(true) );
+		}
 
-        static BruteForce _instance;
-        public static BruteForce Instance
-        {
-            get
-            {
-                return LazyInitializer.EnsureInitialized(ref _instance);
-            }
-        }
+		protected override bool IsPrimeInternal(ulong value)
+		{
+			return Values()
+				.Skip(2) // Is prime handles 2 and 3.
+				.TakeWhile(p => p <= value)
+				.Any(p => p == value);
+		}
 
-    }
+		static BruteForce _instance;
+		public static BruteForce Instance
+		{
+			get
+			{
+				return LazyInitializer.EnsureInitialized(ref _instance);
+			}
+		}
 
-    static PrimeNumbers _default;
-    public static PrimeNumbers Default
-    {
-        get
-        {
-            return LazyInitializer.EnsureInitialized(ref _default);
-        }
-    }
+	}
+
+	static PrimeNumbers _default;
+	public static PrimeNumbers Default
+	{
+		get
+		{
+			return LazyInitializer.EnsureInitialized(ref _default);
+		}
+	}
 
 
 }
--- Program.cs
+++ Program.cs
@@ -1,25 +1,77 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ParallelProcessing
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var optimized = PrimeNumbers.Default;
-            var bruteforce = PrimeNumbers.BruteForce.Instance;
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var sw = Stopwatch.StartNew();
 
-            ulong last = 0;
-            foreach(var p in optimized.Values())
-            {
-                Console.WriteLine("{0} {1}",p , p-last);
-                last = p;
-                if(!bruteforce.IsPrime(p))
-                {
-                    Console.WriteLine("^^^^ is not prime");
-                    return;
-                }
-            }
-        }
-    }
+			var canceller = new CancellationTokenSource();
+			var worker = Task.Run(() => DiscoverPrimePercent(canceller.Token, 1000000000))
+				.ContinueWith(s => Console.WriteLine("Elapsed: {0}", sw.Elapsed));
+
+			Console.ReadLine();
+			canceller.Cancel();
+
+			worker.Wait();
+		}
+
+		static void DiscoverPrimeMaxDeltas(CancellationToken token)
+		{
+
+			var optimized = PrimeNumbers.Default;
+			var bruteforce = PrimeNumbers.BruteForce.Instance;
+
+			ulong maxDelta = 0;
+			ulong last = 0;
+			foreach (var p in optimized.Values())//.TakeWhile(v=>v<100000))
+			{
+				if (token.IsCancellationRequested)
+					break;
+				// if (!bruteforce.IsPrime(p)) // Verify
+				// {
+				// 	Console.WriteLine("{0} is not prime", p);
+				// 	break;
+				// }
+				var delta = p - last;
+				if (delta > maxDelta)
+				{
+					Console.Write(
+						"{0} - {1} = {2}",
+						p, last, delta);
+					Console.Write(
+						", {0} / 2 = {1}",
+						delta / 2, maxDelta / 2);
+					Console.Write(
+						", {0} - {1} = {2}",
+						delta / 2, maxDelta / 2, delta / 2 - maxDelta / 2);
+					Console.WriteLine();
+					maxDelta = delta;
+				}
+				last = p;
+			}
+		}
+
+		static void DiscoverPrimePercent(CancellationToken token, ulong maxCount = ulong.MaxValue)
+		{
+
+			var optimized = PrimeNumbers.Default;
+
+			foreach (var p in optimized.IndexedValues())
+			{
+				if (p.Key >= maxCount || token.IsCancellationRequested)
+				{
+					Console.WriteLine("{0} / {1} = {2:0.000%}", p.Key, p.Value, ((double)p.Key) / p.Value);
+					break;
+				}
+			};
+
+		}
+	}
 }
